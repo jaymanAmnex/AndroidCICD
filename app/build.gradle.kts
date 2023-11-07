584b1bc6c73572058333d310665e5d2af31a5d61
jayman.jani <jayman@amnex.com>
2023-11-07 13:31:14 +0530

    Path updated For APK Build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.androidcicd"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidcicd"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
